83b7d09306ebbcbd93b5d769bc44a40dab8247f9
Khoi Do <khoizpro31@gmail.com>
2024-02-11 01:01:46 +0700

    Add Color change and deletion

---
+++ app/board/[boardId]/_components/color-picker.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { colorToCss } from "@/lib/utils";
+import { Color } from "@/types/canvas";
+
+interface ColorPickerProps {
+  onChange: (color: Color) => void;
+}
+
+export const ColorPicker = ({ onChange }: ColorPickerProps) => {
+  return (
+    <div className="flex max-w-[164px] flex-wrap items-center gap-2 border-r border-neutral-200 pr-2">
+      <ColorButton color={{ r: 243, g: 82, b: 35 }} onClick={onChange} />
+      <ColorButton color={{ r: 255, g: 249, b: 177 }} onClick={onChange} />
+      <ColorButton color={{ r: 68, g: 202, b: 99 }} onClick={onChange} />
+      <ColorButton color={{ r: 39, g: 142, b: 237 }} onClick={onChange} />
+      <ColorButton color={{ r: 155, g: 105, b: 245 }} onClick={onChange} />
+      <ColorButton color={{ r: 252, g: 142, b: 42 }} onClick={onChange} />
+      <ColorButton color={{ r: 0, g: 0, b: 0 }} onClick={onChange} />
+      <ColorButton color={{ r: 255, g: 255, b: 255 }} onClick={onChange} />
+    </div>
+  );
+};
+
+interface ColorButtonProps {
+  onClick: (color: Color) => void;
+  color: Color;
+}
+
+const ColorButton = ({ color, onClick }: ColorButtonProps) => {
+  return (
+    <button
+      onClick={() => onClick(color)}
+      className="flex h-8 w-8 items-center justify-center transition hover:opacity-75"
+    >
+      <div
+        className="h-8 w-8 rounded-md border border-neutral-300 "
+        style={{
+          background: colorToCss(color),
+        }}
+      />
+    </button>
+  );
+};
---
+++ app/board/[boardId]/_components/selection-tool.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useSelectionBounds } from "@/hooks/use-selection-bounds";
+import { useMutation, useSelf } from "@/liveblocks.config";
+import { Camera, Color } from "@/types/canvas";
+import { memo } from "react";
+import { ColorPicker } from "./color-picker";
+import { useDeleteLayers } from "@/hooks/use-delete-layer";
+
+import { Button } from "@/components/ui/button";
+import { Hint } from "@/components/hint";
+import { Trash2 } from "lucide-react";
+
+interface SelectionToolsProps {
+  camera: Camera;
+  setLastUsedColor: (color: Color) => void;
+}
+
+export const SelectionTools = memo(
+  ({ camera, setLastUsedColor }: SelectionToolsProps) => {
+    const selection = useSelf((me) => me.presence.selection);
+
+    const setFill = useMutation(
+      ({ storage }, fill: Color) => {
+        const liveLayers = storage.get("layers");
+        setLastUsedColor(fill);
+
+        selection.forEach((id) => {
+          liveLayers.get(id)?.set("fill", fill);
+        });
+      },
+      [selection, setLastUsedColor],
+    );
+
+    // delete layer
+    const deleteLayers = useDeleteLayers();
+
+    const selectionBounds = useSelectionBounds();
+
+    if (!selectionBounds) return null;
+
+    const x = selectionBounds.width / 2 + selectionBounds.x + camera.x;
+    const y = selectionBounds.y + camera.y;
+
+    return (
+      <div
+        className="absolute flex select-none rounded-xl border bg-white p-3 shadow-sm"
+        style={{
+          transform: `translate(calc(${x}px - 50%), calc(${y - 16}px - 100%))`,
+        }}
+      >
+        <ColorPicker onChange={setFill} />
+
+        <div className="ml-2 flex items-center border-l border-neutral-200 pl-2">
+          <Button variant="board" size="icon" onClick={deleteLayers}>
+            <Trash2 />
+          </Button>
+        </div>
+      </div>
+    );
+  },
+);
+
+SelectionTools.displayName = "SelectionTools";
